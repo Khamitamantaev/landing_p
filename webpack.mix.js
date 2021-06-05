4e9e81274e0735fdf1cfd6d94e9e3b1da10cbdc8
Khamit Amantaev <khamitamantaev@gmail.com>
2021-06-05 16:17:46 +0500

    загрузил img и настроил src пути до public файла

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,28 @@
+const { css } = require("laravel-mix");
 let mix = require("laravel-mix");
 require("mix-html-builder");
 mix.override((config) => {
   delete config.watchOptions;
 }); // игнорирование node_modules файлов на просмотр изменений
 mix
-  .js("resources/js/app.js", "public/js")
-  .sass("resources/sass/app.scss", "public/css")
+  .js("src/resources/js/app.js", "public/js")
+  .sass("src/resources/sass/app.scss", "public/css")
+  .webpackConfig({
+    module: {
+      rules: [
+        {
+          test: /(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/,
+          loaders: {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]?[hash]",
+              context: "src/resources",
+            },
+          },
+        },
+      ],
+    },
+  })
   .browserSync({
     // proxy: "localhost:8080",
     watch: true,
@@ -15,19 +32,20 @@ mix
       "./public/css/**/*.css",
       "./public/js/**/*.js",
       "./fonts/**/*.css",
-      "./resources/index.html",
+      "./src/resources/index.html",
     ],
   });
 
-mix.copyDirectory("resources/assets/fonts", "public/fonts");
-mix.copy("resources/css/reset.css", "public/css");
+mix.copyDirectory("src/resources/fonts", "public/fonts");
+mix.copyDirectory("src/resources/img", "public/img/", false);
+mix.copy("src/resources/css/reset.css", "public/css");
 
 mix.html({
-  htmlRoot: "./resources/index.html", // Your html root file(s)
+  htmlRoot: "./src/resources/index.html", // Your html root file(s)
   output: "public", // The html output folder
   // inject: true,
-  // partialRoot: "./src/partials", // default partial path
-  // layoutRoot: "./src/layouts", // default partial path
+  partialRoot: "./src/resources/partials", // default partial path
+  layoutRoot: "./src/resources/layouts", // default partial path
   minify: {
     removeComments: true,
   },
